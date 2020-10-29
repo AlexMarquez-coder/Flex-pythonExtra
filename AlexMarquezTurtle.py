972dc06956953c6a5138ca0420df885660388f76
Alex Marquez <70955507+AlexMarquez-coder@users.noreply.github.com>
2020-10-29 10:53:20 +0100

    Add files via upload

---
+++ AlexMarquezTurtle.py
@@ -0,0 +1,29 @@
+import turtle
+
+turtle.speed(0)
+turtle.bgcolor("black")
+
+straal = 10
+
+for x in range(1440):
+    turtle.color("blue")
+    turtle.circle(150)
+    turtle.left(1)
+    turtle.color("green")
+    turtle.circle(150)
+    turtle.left(1)
+    turtle.color("red")
+    turtle.circle(150)
+    turtle.left(1)
+    turtle.color("yellow")
+    turtle.circle(150)
+    turtle.left(1)
+
+
+turtle.done()
+
+    
+    
+    
+    
+    
